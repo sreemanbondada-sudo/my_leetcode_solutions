2d90c56522c5516f44090d34c5c03bc196eb78b8
Sreeman Bondada <sreeman.bondada@gmail.com>
2026-04-03 21:47:31 +0530

    Time: 355 ms (40.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,20 @@
+WITH DailyTotals AS (
+    SELECT 
+        visited_on,
+        SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+)
+SELECT 
+    d1.visited_on,
+    SUM(d2.amount) AS amount,
+    ROUND(SUM(d2.amount) / 7, 2) AS average_amount
+FROM DailyTotals d1
+JOIN DailyTotals d2
+  ON d2.visited_on BETWEEN DATE_SUB(d1.visited_on, INTERVAL 6 DAY) AND d1.visited_on
+GROUP BY d1.visited_on
+HAVING COUNT(DISTINCT d2.visited_on) = 7
+ORDER BY d1.visited_on;
+
+
+
