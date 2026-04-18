4eac394cc77f32c6d7d0660b46eb60a107daae9d
Sreeman Bondada <sreeman.bondada@gmail.com>
2026-04-18 23:15:50 +0530

    Time: 493 ms (79.32%) | Memory: 0B (100.00%) - LeetSync

--- 1948-rearrange-products-table/rearrange-products-table.sql
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -1,14 +1,11 @@
 # Write your MySQL query statement below
-SELECT product_id, 'store1' AS store, store1 AS price
+SELECT product_id, 'store1' AS store, store1 as price
 FROM Products
 WHERE store1 IS NOT NULL
-
 UNION ALL
-
 SELECT product_id, 'store2' AS store, store2 AS price
 FROM Products
-WHERE store2 IS NOT NULL
-
+WHERE store2 is not null
 UNION ALL
 
 SELECT product_id, 'store3' AS store, store3 AS price
