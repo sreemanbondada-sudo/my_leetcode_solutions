6f714938229a9db85cb6da8fe175e9ccb88b8e1a
Sreeman Bondada <sreeman.bondada@gmail.com>
2026-07-29 19:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (8.63%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n;
+        n = digits.length;
+        int i;
+        for(i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] result = new int[n+1];
+        result[0] = 1;
+        return result;
+    }
+}
